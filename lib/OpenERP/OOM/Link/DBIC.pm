e7b7f2a4bcf53b93c3eaa417116c65393eea4eaa
JJ <jj@opusvl.com>
2011-11-19 16:47:37 +0000

    Remove the _source() class method when a linked DBIC object is created

--- lib/OpenERP/OOM/Link/DBIC.pm
+++ lib/OpenERP/OOM/Link/DBIC.pm
@@ -27,6 +27,13 @@ sub create {
     my ($self, $args, $data) = @_;
     
     try {
+        if ($self->dbic_schema->resultset($args->{class})->result_class->can('_source')) {
+            # Because the _source() method is added to the result class, we need
+            # to remove it in case it gets called during create(). If that happens,
+            # we get bleed-through from one object to another (not good!)
+            $self->dbic_schema->resultset($args->{class})->result_class->meta->make_mutable;
+            $self->dbic_schema->resultset($args->{class})->result_class->meta->remove_method('_source');
+        }
         my $object = $self->dbic_schema->resultset($args->{class})->create($data);
         warn "Created linked object with ID " . $object->id;
         return $object->id;
