a733d4ee517360cb1e9512ba182ac17ef06bca86
Colin Newell <colin@opusvl.com>
2012-04-05 16:11:06 +0100

    Added documentation

--- lib/OpenERP/OOM/Schema.pm
+++ lib/OpenERP/OOM/Schema.pm
@@ -1,4 +1,58 @@
 package OpenERP::OOM::Schema;
+=head1 NAME
+
+OpenERP::OOM::Schema
+
+=head1 SYNOPSYS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 class
+
+Provides the OpenERP::OOM::Class class object requested.  This will
+load the class into memory.
+
+    my $accounts = $schema->class('Accounts');
+
+=head1 PROPERTIES
+
+=head2 openerp_connect
+
+This should be populated with a hash of the OpenERP connection details.
+
+        <openerp_connect>
+            username admin
+            password admin
+            dbname company-database
+            host openerp-server
+        </openerp_connect>
+
+=head2 link_provider
+
+Out of the box the links to the external database are generated each time the 
+link is followed.  It's generally a good idea to provide your own provider that
+provides a cached connection for the link.
+
+NOTE: this could do with more detail to explain it better.
+
+=head2 link_config
+
+This is the configuration for the externals links.  Typcially it is setup with
+the connection information if the default link provider is used.  If another link
+provider is provided this won't be necessary.
+
+=head2 client
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (C) 2011 OpusVL
+
+This software is licensed according to the "IP Assignment Schedule"
+provided with the development project.
+
+=cut
 
 use 5.010;
 use Moose;
